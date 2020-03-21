eb03c6cc4805722b6d7a68f2b354159db457668e
Muhammad Surya Maulana <battletank19@gmail.com>
2020-03-21 17:43:28 +0700

    add og meta, favicon, thumbnail.
    add og meta, favicon, thumbnail.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,8 +4,17 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=yes" name="viewport">
     <title>The Journey of Covid-19 in Indonesia - Peduli Corona</title>
-    <meta name="description" content="Statistik Penyebaran Virus Corona di Indonesia">
-    <meta name="author" content="Alfan Jauhari">
+    <meta name="description" content="Live Data & Statistik Penyebaran Virus Corona di Indonesia dan Dunia">
+    <meta name="author" content="Peduli Corona">
+
+    <meta property="og:title" content="The Journey of Covid-19 in Indonesia - Peduli Corona" />
+    <meta property="og:description" content="Live Data & Penyebaran Coronavirus (Covid-19) Di Indonesia Dan Dunia" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://pedulicorona.com/" />
+    <meta property="og:image" content="{{ asset('images/thumbnail.jpg') }}" />
+
+    <link rel="canonical" href="https://pedulicorona.com/">
+    <link rel="icon" href="{{ asset('fav.png') }}">
 
     <!-- IonIcons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
